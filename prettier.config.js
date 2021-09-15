17bc15aa33a14cb482f07f9e6ae3a347ec30ec98
mparulski <michal.parulski@gmail.com>
2021-09-15 19:43:04 +0200

    upgrade prettier to 2.4.0

--- prettier.config.js
+++ prettier.config.js
@@ -1,11 +1,12 @@
 module.exports = {
   arrowParens: 'avoid',
+  bracketSameLine: false,
   bracketSpacing: false,
   embeddedLanguageFormatting: 'auto',
   endOfLine: 'lf',
   htmlWhitespaceSensitivity: 'css',
   insertPragma: false,
-  printWidth: 80,
+  printWidth: 120,
   proseWrap: 'always',
   quoteProps: 'as-needed',
   requirePragma: false,
